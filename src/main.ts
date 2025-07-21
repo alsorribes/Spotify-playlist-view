33beb6174a5dd894f03aaa217331fcb612c1da68
Albert <alsorribes@gmail.com>
2025-07-21 09:41:36 +0200

    Implemntació de addIcons per implementar icones i topbar implementada al 100%

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
 import { App } from './app/app';
 import { routes } from './app/app.routes';
+import { addIcons } from 'ionicons';
+import { home, homeOutline, searchOutline, micOutline, notificationsOutline, downloadOutline, chevronDownOutline } from 'ionicons/icons';
 
 bootstrapApplication(App, {
   providers: [
@@ -10,4 +12,14 @@ bootstrapApplication(App, {
     provideIonicAngular(),
     provideRouter(routes),
   ],
+})
+
+addIcons({
+  'home': home,
+  'home-outline': homeOutline,
+  'search-outline': searchOutline,
+  'mic-outline': micOutline,
+  'notifications-outline': notificationsOutline,
+  'download-outline': downloadOutline,
+  'chevron-down-outline': chevronDownOutline
 });
